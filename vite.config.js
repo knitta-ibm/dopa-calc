2fedb6b84184eee889748f52ef2d6c501da88343
Keisuke Nitta <knitta@jp.ibm.com>
2026-09-23 20:37:59 +0900

    feat: add GitHub Pages deployment with PWA support

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/dopa-calc/',
   plugins: [
     react(),
     VitePWA({
@@ -16,8 +17,8 @@ export default defineConfig({
         background_color: '#1a1a2e',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
-        scope: '/',
+        start_url: '/dopa-calc/',
+        scope: '/dopa-calc/',
         icons: [
           {
             src: 'pwa-192x192.png',
